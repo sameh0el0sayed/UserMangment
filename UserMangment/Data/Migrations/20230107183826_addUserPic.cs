d7c628f2f32f7cfd124db2b9e4c0a98790c3eb2e
sameh_urban <sameh@urbansoft.co>
2023-01-08 11:37:02 +0300

    first

---
+++ UserMangment/Data/Migrations/20230107183826_addUserPic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserMangment.Data.Migrations
+{
+    public partial class addUserPic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePicture",
+                schema: "Security",
+                table: "User",
+                type: "varbinary(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                schema: "Security",
+                table: "User");
+        }
+    }
+}
