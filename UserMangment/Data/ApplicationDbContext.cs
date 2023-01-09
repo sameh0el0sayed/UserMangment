33cfe6fef52c0324111cadf8235c9944f96ad9f6
sameh_urban <sameh@urbansoft.co>
2023-01-09 13:17:08 +0300

    Convert to MVC

---
+++ UserMangment/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserMangment.Models;
+
+namespace UserMangment.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>().ToTable("User", "Security");
+            builder.Entity<IdentityRole>().ToTable("Role", "Security");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRole", "Security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim", "Security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin", "Security");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim", "Security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserToken", "Security");
+
+        }
+    }
+}
---
+++ UserMangment/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UserMangment.Data;
+using UserMangment.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+    options.SignIn.RequireConfirmedEmail = true;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddHealthChecks();
+builder.Services.AddMvc();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+ app.UseRouting();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
